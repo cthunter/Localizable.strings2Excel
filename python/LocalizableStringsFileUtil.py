a9c6ccee4aeb01571716537b8c009d2bc2be517a
chentao <chentao@ukelink.com>
2017-07-21 10:19:12 +0800

    replace keys from xls

--- python/LocalizableStringsFileUtil.py
+++ python/LocalizableStringsFileUtil.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import os
+import os,shutil
 from Log import Log
 import codecs
+from fuzzywuzzy import process
 
 class LocalizableStringsFileUtil:
     'iOS Localizable.strings file util'
@@ -60,3 +61,44 @@ class LocalizableStringsFileUtil:
                 values.append(value)
 
         return (keys,values)
+
+    @staticmethod
+    def fuzzyReplaceLocalizableFile(keys,values,directory,additional):
+        if not os.path.exists(directory):
+            return
+
+        Log.info("Open iOS file:" + directory+"Localizable.strings")
+        fn = directory+"Localizable.strings"
+        shutil.copyfile(fn,fn+".bak")
+
+        fo = open(directory+"KeyMap.txt", "wb")
+        (okeys,ovalues) = LocalizableStringsFileUtil.getKeysAndValues(fn)
+        
+
+        for x in range(len(values)):
+            if values[x] is None or values[x] == '':
+                Log.error("Key:" + keys[x] + "\'s value is None. Index:" + str(x + 1))
+                continue
+
+            key = keys[x]
+            value = values[x]
+
+            (ovalue,ratio) = process.extractOne(value,ovalues)
+
+            if ratio > 80 :
+                oindex = ovalues.index(ovalue)
+                Log.info("found a string '"+ovalue+"' with key '"+key+"' index: "+str(oindex))
+                content = okeys[oindex] + ","+ key + "\n"
+                fo.write(content);
+
+        fo.close
+
+        LocalizableStringsFileUtil.writeToFile(keys,values,directory,additional)        
+
+            # content = "\"" + key + "\" " + "= " + "\"" + value + "\";\n"
+            # fo.write(content);
+
+        # if additional is not None:
+        #     fo.write(additional)
+
+        # fo.close()
